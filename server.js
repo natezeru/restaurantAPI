56de80576108f44c74ce7148ebc0ed5a831b77f2
Zeru <zeruna@ad.vzwcorp.com>
2021-07-18 04:13:37 -0500

    app almost done

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 
-app.use(express.json())  //understand path.join
+app.use(express.json())  //understand path.join  //adding body parser
 
 //route to all Restaurants
 app.get('/restaurant', async(req,res) =>{
@@ -22,11 +22,35 @@ app.get('/restaurant/:id', async(req,res) => {
   res.json({restaurant})
 })
 
+
+//create a new restarurant 
+
+app.post('/restaurant', async(req,res) =>{
+  let restarurant = await Restaurant.create()
+  res.send("New Restaurant Created") //res.send to send message to client that its created
+})
+
+// delete a restaurant
+
+app.delete('/restaurant/:id', async(req, res) => {
+  await Restaurant.destroy({
+    where: { id: req.params.id}  //destroying restaurant where id is matched
+  })
+  res.send("deleted restaurant")
+})
 //get restaurant and assoication  //got menus associated with restaurant how do i get items aswell - ask dan
 
 app.get('/restaurantAssociation/:id', async(req,res) => {
   let restaurant = await Restaurant.findByPk(req.params.id, {include: Menu}, {include: Item});
   res.json({restaurant})
+})
+
+//update restaurant based on id 
+
+app.put('/restaurant/:id', async(req, res) => {
+  let updated = await Restaurant.update(req.body, {
+    where: {id: req.params.id}   //update restaurant where id matches , the req.body
+  })     //req.body will tell us what the updates are... the req.params tell us what restaurant gets updated
 })
 
 
@@ -41,16 +65,29 @@ app.get("/menu/:id", async(req,res) => {
   let menu = await Menu.findByPk(req.params.id)
   res.json({menu})
 })
+//update menu by id 
+app.put('/menu/:id', async(req, res) => {
+  let updated = await Menu.update(req.body, {
+    where: {id: req.params.id}   //update menu where id matches , the req.body
+  })     //req.body will tell us what the updates are... the req.params tell us what restaurant gets updated
+})
 
-//get menu and association
 
 
 //create menu
+app.post('/menu', async(req,res) => {
+  let menu = await Menu.create()
+  res.send('menu created')
+})
 
 
-
-//delete menu
-
+//delete menu by id
+app.delete('/menu/:id', async(req, res) => {
+  await Menu.destroy({
+    where: { id: req.params.id}  //destroying menu where id is matched
+  })
+  res.send("deleted menu")
+})
 
 
 //route to all Items
@@ -71,8 +108,29 @@ app.get('/itemAssociation/:id', async(req,res) => {
   res.json({item})
 })
 
+//delete item by id
 
+app.delete('/item/:id', async(req, res) => {
+  await Item.destroy({
+    where: { id: req.params.id}  //destroying item where id is matched
+  })
+  res.send("deleted item")
+})
 
+//create an item
+
+app.post("/item", async(req,res) => {
+  let item = await Item.create()
+  res.json("item created")
+})
+
+//update item by id 
+app.put('/item/:id', async(req, res) => {
+  let updated = await Item.update(req.body, {
+    where: {id: req.params.id}   //update item where id matches , the req.body
+  })     //req.body will tell us what the updates are... the req.params tell us what restaurant gets updated
+  res.send("Item updated")
+})
 
 // listen
 app.listen(port, () => {
